d268cece9d89e285b5b5232ec9ddb49a277fd572
devychen <119744412+devychen@users.noreply.github.com>
2024-08-01 15:47:33 +0200

    Complete lab5

---
+++ Labs_S1/lab5/Person.java
@@ -0,0 +1,166 @@
+/**
+ * @author: mkz
+ * Class description:
+ *  Class for a Person.
+ *
+ *  Pre-conditions:
+ *  For set methods: the person has been created.
+ *  For test methods: the persons have been created and have values for name and age.
+ *
+ *  Post-conditions:
+ *  Set methods: one or both attributes is assigned a value.
+ *  Test methods: true or false is returned.
+ *
+ *  Based on: Self-Test Question #13 in Chapter 4 (pp. 289-90).
+ */
+
+
+import java.util.*;
+
+public class Person
+{
+    private String name;
+    private int age;
+    
+    /**
+     * Initializes a Person. Reads name and age of a person 
+     * from System.in. 
+     */
+    public void readInput()
+    {
+        // Create a scanner to ...
+        Scanner keyboard = new Scanner(System.in);
+        
+        // read name...
+        System.out.println("What is the person's name?");
+        name = keyboard.nextLine();
+        
+        // and age from System.in
+        System.out.println("What is the person's age?");
+        age = keyboard.nextInt();
+        while (age < 0)
+        {
+            // Make sure that age is not negative!!!
+            System.out.println("Age cannot be negative.");
+            System.out.println("Reenter age:");
+            age = keyboard.nextInt();
+        }
+    }
+    
+    /**
+     * Print a description of this Person to System.out
+     */
+    public void writeOutput()
+    {
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+    }
+    
+    /**
+     * Initializes this Person with name <i>newName</i> and age
+     * <i>newAge</i>
+     * @param newName - new name of this Person
+     * @param newAge  - new age of this Person
+     */
+    public void set(String newName, int newAge)
+    {
+        setName(newName);  //Set name...
+        setAge(newAge);    // and age         
+    }
+    
+    /**
+     * Sets the name of this Person to <i>newName</i>
+     * @param newName - new name of this Person
+     */
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+    
+    /**
+     * Sets the new age of this Person
+     * @param newAge  - new age of this Person
+     */
+    public void setAge(int newAge)
+    {
+        if (newAge >= 0)
+            age = newAge;
+        else
+        {
+            System.out.println("ERROR: Used a negative age.");
+            System.exit(0);
+        }
+    }
+    
+    /**
+     * Returns the name of this Person
+     * @return The name of this Person
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     * Returns the age of this Person
+     *@return The age of this Person
+     */
+    public int getAge()
+    {
+        return age;
+    }
+
+
+    // TODO : if same person
+
+    /**
+     * @param anotherPerson
+     * @return True if the same person
+     */
+    public boolean equals(Person anotherPerson){
+        return this.name.equalsIgnoreCase(anotherPerson.getName()) && age == anotherPerson.getAge();
+    }
+
+
+    // TODO: if same name
+
+    /**
+     * @param anotherPerson
+     * @return True if same name
+     */
+    public boolean isSameName(Person anotherPerson){
+        return this.name.equalsIgnoreCase(anotherPerson.getName());
+    }
+
+    // TODO: if same age
+
+    /**
+     * @param anotherPerson
+     * @return True if same age
+     */
+    public boolean isSameAge(Person anotherPerson){
+        return age == anotherPerson.getAge();
+    }
+
+    // TODO: if the instance is older than anotherPerson
+
+    /**
+     * @param anotherPerson
+     * @return True if older
+     */
+    public boolean isOlderThan(Person anotherPerson){
+        return age > anotherPerson.getAge();
+    }
+
+    // TODO: if the instance is younger than anotherPerson.
+    /**
+     * @param anotherPerson
+     * @return True if younger
+     */
+    public boolean isYoungerThan(Person anotherPerson){
+      return age < anotherPerson.getAge();
+    }
+
+
+}
+
---
+++ Labs_S1/lab5/PersonTest.java
@@ -0,0 +1,119 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * A JUnit test case class. Every method starting with the word "test" will be
+ * called when running the test with JUnit.
+ */
+public class PersonTest {
+
+    /**
+     * Tests for the same age (Expect true)
+     */
+    @Test
+    public void testSameAge1() {
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Jim Knopf", 12);
+        assertTrue(testPerson.isSameAge(testPerson2));
+    }
+
+    /**
+     * Tests for the same age (Expect false)
+     */
+    @Test
+    public void testSameAge2() {
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Lukas Lokomotivfuehrer", 38);
+        assertFalse(testPerson.isSameAge(testPerson2));
+    }
+
+    // TODO: More tests to follow.....
+
+    /**
+     * Test for the same name
+     */
+    @Test
+    public void testSameName1() {
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Jim Knopf", 38);
+        assertTrue(testPerson.isSameName(testPerson2));
+    }
+
+    public void testSameName2() {
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Lukas Lokomotivfuehrer", 38);
+        assertFalse(testPerson.isSameName(testPerson2));
+    }
+
+    /**
+     * Test for the same person
+     */
+    @Test
+    public void testSamePerson1(){
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Jim Knopf", 12);
+        assertTrue(testPerson.equals(testPerson2));
+    }
+
+    @Test
+    public void testSamePerson2(){
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Lukas Lokomotivfuehrer", 38);
+        assertFalse(testPerson.equals(testPerson2));
+    }
+
+    /**
+     * Test for the instance is older
+     */
+    @Test
+    public void testOlder1(){
+        Person testPerson = new Person();
+        testPerson.set("Lukas Lokomotivfuehrer", 38);
+        Person testPerson2 = new Person();
+        testPerson2.set("Jim Knopf", 12);
+        assertTrue(testPerson.isOlderThan(testPerson2));
+    }
+
+    @Test
+    public void testOlder2(){
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Lukas Lokomotivfuehrer", 38);
+        assertFalse(testPerson.isOlderThan(testPerson2));
+    }
+
+    /**
+     * Test for the instance is younger
+     */
+    @Test
+    public void testYounger1(){
+        Person testPerson = new Person();
+        testPerson.set("Jim Knopf", 12);
+        Person testPerson2 = new Person();
+        testPerson2.set("Lukas Lokomotivfuehrer", 38);
+        assertTrue(testPerson.isYoungerThan(testPerson2));
+    }
+
+    @Test
+    public void testYounger2(){
+        Person testPerson = new Person();
+        testPerson.set("Lukas Lokomotivfuehrer", 38);
+        Person testPerson2 = new Person();
+        testPerson2.set("Jim Knopf", 12);
+        assertFalse(testPerson.isYoungerThan(testPerson2));
+    }
+}
