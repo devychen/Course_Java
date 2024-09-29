d6d705089099303f347acaa0a8771d3576262b84
devychen <119744412+devychen@users.noreply.github.com>
2024-09-29 18:23:43 +0200

    update project structure

---
+++ Labs_S2/lab10_Extra/README.md
@@ -0,0 +1,30 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/USsM_LT3)
+# Lab 10
+
+The deadline is **16:00 on Friday, July 5**. Submissions later than this time will be 
+ignored. Be sure to push your commits before the deadline.
+
+## Exercise 1
+
+`addresses.xml` contains addresses in a simple XML encoding:
+
+```xml
+<addressbook>
+    <address>
+        <forename>x</forename>
+        <surname>y</surname>
+        <email>z</email>
+    </address>
+</addressbook>
+```
+
+`Addressbook.java` contains code to load and save instances of class 
+`Address.java` to files. Please complete the missing code. Use `AddressbookTest.java` to test your code. 
+
+Your methods **must** use the STAX API to read and write files. 
+
+Do not change the given code. Instead, you should interface your methods to the provided methods.
+
+Remember that your output file should have proper formatting and indentation.
+
+
---
+++ Labs_S2/lab10_Extra/dsa2_lab10/Addressbook.java
@@ -0,0 +1,91 @@
+package dsa2_lab10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * @author Saile
+ */
+
+public class Addressbook {
+
+    private String addressFileName;
+    private ArrayList<Address> addresses = new ArrayList<Address>();
+
+    public Addressbook(String addressFileName) throws XMLStreamException, FileNotFoundException {
+        this.addressFileName = addressFileName;
+        load();
+    }
+    
+    /**
+     * Return an ArrayList with the Addresses of this instance
+     * @return the addresses
+     */
+    public ArrayList<Address> getAddresses() {
+        return addresses;
+    }
+
+    /**
+     * Return the name of the file of this Addressbook
+     * @return the addressFileName
+     */
+    public String getAddressFileName() {
+        return addressFileName;
+    }
+
+    /**
+     * Set the name of the file of this Adressbook
+     * @param addressFileName the addressFileName to set
+     */
+    public void setAddressFileName(String addressFileName) {
+        this.addressFileName = addressFileName;
+    }
+
+    /**
+     * Save the content to addressFile
+     * @param addressFile path to the file the content is written to
+     */
+    public void saveToFile(String addressFile) throws IOException {
+        FileWriter fw = new FileWriter(new File(addressFile));
+        generateXML(fw);
+        fw.close();
+    }
+
+    /**
+     * Save to data file
+     */
+    public void save() throws IOException {
+        saveToFile(addressFileName);
+    }
+
+    /**
+     * Return a String with the XML representation of this instance
+     * @return a String with the XML representation of this instance
+     */
+    public String toString() {
+        StringWriter sw = new StringWriter();
+        generateXML(sw);
+        return sw.toString();
+    }
+
+    /**
+     * TODO - You are allowed to change the method signature.
+     */
+    private void load() {
+
+    }
+
+    /**
+     * TODO
+     * @param w
+     */
+    private void generateXML(Writer w) {
+
+    }
+}
