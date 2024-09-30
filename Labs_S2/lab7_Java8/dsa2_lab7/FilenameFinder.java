d4de28650962723fa0c4234be5b8af6a7bed42da
devychen <119744412+devychen@users.noreply.github.com>
2024-09-30 12:37:46 +0200

    Update FilenameFinder.java

--- Labs_S2/lab7_Java8/dsa2_lab7/FilenameFinder.java
+++ Labs_S2/lab7_Java8/dsa2_lab7/FilenameFinder.java
@@ -3,24 +3,27 @@ package dsa2_lab7;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate; // added
 
 /**
  * @author mkz
  * Task: Instead of using `equals()` to decide when a file name matches the parameter {entryName},
  *      Please rewrite your FilenameFinder and use a Predicate as a criteria for file name selection. 
- *      You can use ur previous FilenameFinder or the below codes provided.
+ *      You can use ur previous FilenameFinder or the codes provided.
  */
 
 public class FilenameFinder {
     /**
-     * Collect file or directory names equal to {@code entryName} and store the absolute pathname to the found entry
-     * in a List. Start the collection at path {@code path}. If {@code path} is a directory continue collection
-     * in its sub directories.
+     * Collect file or directory names equal to {@code entryName} and 
+     * store the absolute pathname to the found entry in a List. 
+     * Start the collection at path {@code path}. 
+     * If {@code path} is a directory continue collection in its sub directories.
      * @param entryName Name to search for
      * @param path root of search
      * @return List with found items or empty list if nothing was found.
      */
-    public static List<String> findEntryAtPath(String entryName, String path) {
+    // todo: Change to Predicate interface
+    public static List<String> findEntryAtPath(String path, Predicate<String> entryName) {
         List<String> entryList = new ArrayList<>();
 
         File fileOrDir = new File(path);
@@ -31,10 +34,13 @@ public class FilenameFinder {
         return entryList;
     }
 
-    private static void realFind(List<String> entryList, String entryName, File fileOrDir) {
+    // todo: replace `equals()` with Predicate
+    private static void realFind(List<String> entryList, Predicate<String> entryName, File fileOrDir) {
         // If name matches add absolute path to list
-        if (fileOrDir.getName().equals(entryName)) {
-            entryList.add(fileOrDir.getAbsolutePath());
+        if (fileOrDir.isFile()){
+            if (entryName.test(fileOrDir.getName())) {
+             entryList.add(fileOrDir.getAbsolutePath());
+            }
         }
 
         // if entry is a directory search all directory members for entryName
@@ -50,4 +56,11 @@ public class FilenameFinder {
             }
         }
     }
+
+//     // light testing
+//    public static void main(String[] args) {
+//        String path = "resources/snook.moo";
+//        List<String> entryList = FilenameFinder.findEntryAtPath(path, s -> s.equals("snook.moo"));
+//        System.out.println(entryList);
+//    }
 }
